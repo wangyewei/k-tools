ec5c3e5b740631b9f7f89faa8dc2d59dcd9799cc
WangYeWei <826036140@qq.com>
2022-10-12 16:44:35 +0800

    update: Configuring the Rollup environment

---
+++ rollup.config.js
@@ -0,0 +1,69 @@
+import * as fs from 'fs'
+import resolve from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
+import typescript from 'rollup-plugin-typescript2'
+import json from '@rollup/plugin-json'
+import babel from '@rollup/plugin-babel'
+import dts from 'rollup-plugin-dts'
+// import { terser } from 'rollup-plugin-terser'
+
+const entry = 'packages/index.ts'
+const componentsDir = 'packages'
+
+const dirents = fs.readdirSync(componentsDir, { withFileTypes: true });
+const filesNames = dirents
+  .filter(dirent => dirent.isDirectory())
+  .map(dirent => dirent.name);
+
+const toolsEntry = filesNames.map(
+  (name) => `${componentsDir}/${name}/index.ts`
+)
+
+const isProd = process.env.NODE_ENV === 'production'
+const BABEL_ENV = process.env.BABEL_ENV
+
+
+const babelOptions = {
+  presets: ["@babel/preset-env"],
+  extensions: ['.js', '.ts',],
+  exclude: "**/node_modules/**",
+  babelHelpers: 'bundled'
+}
+
+const commonPlugins = [
+  resolve(),
+  commonjs({ sourceMap: !isProd }),
+  typescript(),
+  // babel(babelOptions),
+  json(),
+]
+
+const externalConfig = [
+  // id => /\/__expample__|main.tsx/.test(id),
+  '**/node_modules/**'
+]
+
+const esmOutput = {
+  preserveModules: true,
+}
+
+export default () => {
+  switch (BABEL_ENV) {
+    case 'esm':
+      return [
+        {
+          input: [entry, ...toolsEntry],
+          output: { ...esmOutput, dir: 'dist/', format: 'es' },
+          external: externalConfig,
+          plugins: [...commonPlugins]
+        },
+        {
+          input: [entry, ...toolsEntry],
+          output: { ...esmOutput, dir: 'dist/type', format: 'es' },
+          external: externalConfig,
+          plugins: [...commonPlugins, dts()]
+        }
+      ]
+  }
+}
+
--- scripts/build.js
+++
@@ -1,5 +0,0 @@
-import * as path from 'path'
-import * as fs from 'fs'
-
-const entry = 'packages/index.ts'
-const toolsDir = 'pa'
